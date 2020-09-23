e8f2ecd5b621bf8d8cef00916ee164297643eb49
iLyas <isbkch@gmail.com>
2020-09-22 20:54:33 -0400

    connect with WiFi module

--- src/main.ino
+++ src/main.ino
@@ -1,14 +1,17 @@
 #include <dht11.h>
 #include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
 
-#define DHT11PIN 3
+#define DHT11PIN 4
 dht11 sensor;
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+SoftwareSerial WiFiBoard(2, 3);
 
 void setup() {
   Serial.begin(9600);
+  WiFiBoard.begin(115200);
 
   // set up the LCD's number of columns and rows
   lcd.begin(16, 2);
@@ -30,25 +33,36 @@ void setup() {
 void loop() {
 
   // read from the digital pin
-  int check = sensor.read(DHT11PIN);
+  int   check       = sensor.read(DHT11PIN);
+  float temperature = (float)sensor.temperature;
+  float humidity    = (float)sensor.humidity;
 
   // display Hum on the LCD screen
   lcd.setCursor(0, 0);
   lcd.print("Humidity (%): ");
-  lcd.print((float)sensor.humidity, 2);
+  lcd.print(humidity, 2);
 
   // display Temp on the LCD screen
   lcd.setCursor(0, 1);
   lcd.print("Temp (C): ");
-  lcd.print((float)sensor.temperature, 2);
+  lcd.print(temperature, 2);
 
   // print Hum on the serial monitor
   Serial.print("Humidity (%): ");
-  Serial.println((float)sensor.humidity, 2);
+  Serial.println(humidity, 2);
 
   // print Temp on the serial monitor
   Serial.print("Temperature (C): ");
-  Serial.println((float)sensor.temperature, 2);
+  Serial.println(temperature, 2);
 
+  // sendDataToWiFiBoard(temperature, humidity);
+
+  WiFiBoard.println(111);
+  WiFiBoard.write(111);
   delay(2000); // take measurements every 2 sec
+}
+
+void sendDataToWiFiBoard(float temperature, float humidity)
+{
+  WiFiBoard.println(temperature);
 }
